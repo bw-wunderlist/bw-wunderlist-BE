15a688e50c6dc00fc7591a013c7bd63d3d7c89c0
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-13 12:31:27 -0400

    adding for maks

--- api/server.js
+++ api/server.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const helmet = require('helmet')
-const cors = require('cors')
-const bcrypt = require('bcryptjs')
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
 // DB IMPORT
 const db = require("../data/dbConfig.js");
 // ROUTER IMPORTS
-const usersRouter = require('../users/userRouter.js')
+const usersRouter = require("../users/userRouter.js");
 
 const server = express();
 
 //MIDDLEWARE
 server.use(express.json());
-server.use(cors())
-server.use(helmet())
+server.use(cors());
+server.use(helmet());
 
 // ROUTERS
-server.use('/api/users', usersRouter )
+server.use("/api/users", usersRouter);
 
 //  GET ENDPOINT FOR /
 server.get("/", async (req, res) => {
