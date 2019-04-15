c58e4ca137c430fc1491ef0f34b8a04fea2df57b
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-15 17:45:35 -0400

    Figuring out Schema

--- data/migrations/20190413142134_tasks.js
+++ data/migrations/20190413142134_tasks.js
@@ -2,9 +2,13 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("tasks", tbl => {
     tbl.increments();
     tbl.string("name", 128).notNullable();
-    tbl.string('desc', 255);
-    tbl.date('due_date')
-    tbl.boolean('is_complete').defaultTo(false);
+    tbl.string("desc", 255);
+    //tbl.date("due_date");
+    //tbl.time("time");
+    //tbl.datetime('time_and_date')
+    tbl.boolean("repeat");
+    tbl.string("yyyy-mm-dd", 128).notNullable();
+    tbl.boolean("is_complete").defaultTo(false);
     tbl
       .integer("user_id")
       .unsigned()
@@ -14,6 +18,9 @@ exports.up = function(knex, Promise) {
       .onDelete("CASCADE")
       .onUpdate("CASCADE");
     tbl.timestamps(true, true);
+    //tbl.string("years");
+    //tbl.string("months");
+    //tbl.string("days");
   });
 };
 
--- data/seeds/002_tasks.js
+++ data/seeds/002_tasks.js
@@ -1,7 +1,28 @@
 exports.seed = function(knex, Promise) {
   return knex("tasks").insert([
-    { name: "task", user_id: "DnuzdSXCtMgmRWDCRRE1iQ" },
-    { name: "task2", user_id: "DnuzdSXCtMgmRWDCRRE1iQ" },
-    { name: "task3", user_id: "DnuzdSXCtMgmRWDCRRE1iQ" }
+    {
+      name: "task",
+      user_id: "DnuzdSXCtMgmRWDCRRE1iQ",
+      desc: "task description",
+      "yyyy-mm-dd": "2019-04-15",
+      //time: new Date().toISOString(),
+      repeat: true
+    },
+    {
+      name: "task2",
+      user_id: "DnuzdSXCtMgmRWDCRRE1iQ",
+      desc: "new task desc for task 2",
+      "yyyy-mm-dd": "2019-05-02",
+      //time: new Date().toISOString(),
+      repeat: true
+    },
+    {
+      name: "task3",
+      user_id: "DnuzdSXCtMgmRWDCRRE1iQ",
+      desc: "desc for task 3 going here",
+      "yyyy-mm-dd": "2020-04-15",
+      //time: new Date().toISOString(),
+      repeat: true
+    }
   ]);
 };
