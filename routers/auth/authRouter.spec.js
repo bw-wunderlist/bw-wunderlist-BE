b25bdbff6e8504709051a05e6134f68a887ca47f
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-16 16:16:29 -0400

    AuthRouter.spec test passing on first run.
    believe it is testing as expected. if you run it again it fails. I
    believe it is supposed to do that. added TODO to check with Maks to
    clarify

--- routers/auth/authRouter.spec.js
+++ routers/auth/authRouter.spec.js
@@ -1,15 +1,22 @@
-const request = require('supertest')
-const server = require('../../api/server')
+const request = require("supertest");
+const server = require("../../api/server");
 
-describe('authRouter', () => {
-  describe('register', () => {
-    it.skip('register a user', async() => {
-      const req = await request(server).post('/api/auth/register').send({
-        "username": "user8",
-        "password": "password",
-        "email": "user8@wunderlist.com"
-      }).set('Accept', 'application/json').expect(201)
-      console.log(req.status)
-    })
-  })
-})
+describe("authRouter", () => {
+  describe("register", () => {
+    it.skip("register a user", async () => {
+      const req = await request(server)
+        .post("/api/auth/register")
+        .send({
+          username: "user8",
+          password: "password",
+          email: "user8@wunderlist.com"
+        })
+        .set("Accept", "application/json")
+        .expect(201);
+      console.log(req.status);
+    });
+  });
+});
+
+//TODO; this test past on first attempt and then fails. I believe that is to
+//be expected. will check with Maks
