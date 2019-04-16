29498726eb209652f19a129596284ac93fdfe041
Maksim <maks112v@gmail.com>
2019-04-16 13:13:42 -0400

    Update authRouter.spec.js

---
+++ routers/auth/authRouter.spec.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+const server = require('../../api/server')
+
+describe('authRouter', () => {
+  describe('register', () => {
+    it('register a user', async() => {
+      const req = await request(server).post('/api/auth/register').send({
+        "username": "user8",
+        "password": "password",
+        "email": "user8@wunderlist.com"
+      }).set('Accept', 'application/json').expect(201)
+      console.log(req.status)
+    })
+  })
+})
