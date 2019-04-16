d332a224c58b7a9618c24121fc53bbc97ff05686
Maksim <maks112v@gmail.com>
2019-04-16 12:54:08 -0400

    Update taskModel.spec.js

--- routers/tasks/taskModel.spec.js
+++ routers/tasks/taskModel.spec.js
@@ -1,6 +1,4 @@
-const request = require("supertest");
 const db = require("../../data/dbConfig.js");
-const server = require("../../api/server.js");
 const Tasks = require("./tasksModel.js");
 
 const moment = require('moment')
@@ -14,25 +12,22 @@ describe(`tasksModel`, () => {
     it(`should get id, name, desc, is_complete`, async () => {
       await Tasks.addTask({ name: "test task" }, "DnuzdSXCtMgmRWDCRRE1iQ");
       const tasks = await Tasks.getAllByUserId("DnuzdSXCtMgmRWDCRRE1iQ");
-      console.log(tasks);
       expect(tasks).toHaveLength(1);
     });
   });
 
   describe(`getById`, () => {
     it("should get back single task matching id", async () => {
-      await Tasks.addTask({ name: "testing" }, "DnuzdSXCtMgmRWDCRRE1iQ");
-      const tasks = await Tasks.getById("DnuzdSXCtMgmRWDCRRE1iQ");
-      console.log(tasks);
+      const taskId = await Tasks.addTask({ name: "testing" }, "DnuzdSXCtMgmRWDCRRE1iQ");
+      const tasks = await Tasks.getById(taskId[0]);
       expect(tasks).toBeDefined();
     });
   });
 
   describe("addTask", () => {
-    it.skip(`should add a task`, async () => {
+    it(`should add a task`, async () => {
       await Tasks.addTask({ name: "addtask test" }, "DnuzdSXCtMgmRWDCRRE1iQ");
       const tasks = await Tasks.getAllByUserId("DnuzdSXCtMgmRWDCRRE1iQ");
-      console.log(tasks);
       expect(tasks).toHaveLength(1);
     });
   });
