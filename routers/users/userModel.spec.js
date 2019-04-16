c85ae25bfb902290d9e092883bfdf19a451774de
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-16 16:01:42 -0400

    Added testing for userModel.spec.js

--- routers/users/userModel.spec.js
+++ routers/users/userModel.spec.js
@@ -13,19 +13,53 @@ describe("usersModel", () => {
       const response = await request(server).get("/api/users");
       expect(response.type).toEqual("text/html");
     });
-    it.skip("should return user by id", async () => {
+    it("should return user by id", async () => {
       let uid = "DnuzdSXCtMgmRWDCRRE1iQ";
+      await db("users").insert({
+        uid,
+        username: "jenny",
+        password: "jenny",
+        email: "jenny@test.com"
+      });
       const user = await Users.findById(uid);
       console.log(user);
       expect(user).toBeDefined();
     });
   });
 
-  //describe("updateById()", async => {
-  //it.skip("should update the user info", async () => {});
-  //});
+  describe("updateById()", () => {
+    it("should update the user info", async () => {
+      let uid = "DnuzdSXCtMgmRWDCRRE1iQ";
+      const sam = await db("users").insert({
+        uid,
+        username: "sam",
+        password: "sam",
+        email: "sam@amazon.com"
+      });
+      console.log(sam);
+      const user = await Users.updateById(
+        { email: "philip@microsoft.com" },
+        uid
+      );
+      console.log(user);
+      expect(user).toEqual(1);
+    });
+  });
 
-  //describe("removeUser()", async => {
-  //it.skip("should remove a user", async () => {});
-  //});
+  describe("removeUser()", () => {
+    it("should remove a user", async () => {
+      let uid = "DnuzdSXCtMgmRWDCRRE1iQ";
+      const userGoneTest = await db("users").insert({
+        uid,
+        username: "jenny",
+        password: "jenny",
+        email: "jenny@test.com"
+      });
+      const user = await Users.findById(uid);
+      console.log(user);
+      const gone = await Users.removeUser(uid);
+      console.log(gone);
+      expect(gone).toBe(undefined);
+    });
+  });
 });
