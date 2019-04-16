a8742b08797c1b82f0d9271d0a39ab9896495993
Maksim <maks112v@gmail.com>
2019-04-15 21:03:43 -0400

    fixed migration

--- data/migrations/20190413142134_tasks.js
+++ data/migrations/20190413142134_tasks.js
@@ -2,13 +2,6 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("tasks", tbl => {
     tbl.increments();
     tbl.string("name", 128).notNullable();
-    tbl.string("desc", 255);
-    //tbl.date("due_date");
-    //tbl.time("time");
-    //tbl.datetime('time_and_date')
-    tbl.boolean("repeat");
-    tbl.string("yyyy-mm-dd", 128).notNullable();
-    tbl.boolean("is_complete").defaultTo(false);
     tbl
       .integer("user_id")
       .unsigned()
@@ -17,6 +10,13 @@ exports.up = function(knex, Promise) {
       .inTable("users")
       .onDelete("CASCADE")
       .onUpdate("CASCADE");
+    tbl.string("desc", 255);
+    tbl.boolean("is_complete").defaultTo(false);
+    //tbl.date("due_date");
+    //tbl.time("time");
+    //tbl.datetime('time_and_date')
+    tbl.boolean("repeat");
+    //tbl.string("yyyy-mm-dd", 128).notNullable();
     tbl.timestamps(true, true);
     //tbl.string("years");
     //tbl.string("months");
