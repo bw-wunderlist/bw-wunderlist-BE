b876cfd44d675e328b03d8d74810b98ecae3a690
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-16 12:27:22 -0400

    boilerplating out some test cases

--- routers/tasks/taskModel.spec.js
+++ routers/tasks/taskModel.spec.js
@@ -16,4 +16,42 @@ describe(`tasksModel`, () => {
       expect(tasks).toHaveLength(1);
     });
   });
+
+  describe(`getById`, () => {
+    it("should get back single task matching id", async () => {
+      await Tasks.addTask({ name: "testing" }, "DnuzdSXCtMgmRWDCRRE1iQ");
+      const tasks = await Tasks.getById("DnuzdSXCtMgmRWDCRRE1iQ");
+      console.log(tasks);
+      expect(tasks).toBeDefined();
+    });
+  });
+
+  describe("addTask", () => {
+    it.skip(`should add a task`, async () => {
+      await Tasks.addTask({ name: "addtask test" }, "DnuzdSXCtMgmRWDCRRE1iQ");
+      const tasks = await Tasks.getAllByUserId("DnuzdSXCtMgmRWDCRRE1iQ");
+      console.log(tasks);
+      expect(tasks).toHaveLength(1);
+    });
+  });
+
+  describe("completeById", () => {
+    it.skip("should mark task completed", async () => {
+      console.log("test will go here");
+    });
+  });
+
+  describe("updateTask", () => {
+    it.skip("should update a task", async () => {
+      await Tasks.updateTask();
+    });
+  });
+
+  describe("removeTask", () => {
+    it.skip("should remove task", async () => {
+      await Tasks.removeTask();
+    });
+  });
 });
+
+//TODO: need to fix seeding and migrations to complete testing.
