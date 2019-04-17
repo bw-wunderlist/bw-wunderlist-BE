a8b001bcde41416a6ac9d50851cbff86b93eb20c
Maksim <maks112v@gmail.com>
2019-04-16 20:04:29 -0400

    Update 002_tasks.js

--- data/seeds/002_tasks.js
+++ data/seeds/002_tasks.js
@@ -20,7 +20,7 @@ for (let i = 0; i < 100; i++) {
       number: Math.floor((Math.random() * 365) + 1),
       timeframe: 'days',
       occurrences: Math.floor((Math.random() * 20) + 1),
-    })
+    }),
   })
 }
 
@@ -50,5 +50,6 @@ exports.seed = function(knex, Promise) {
       repeat: 1,
       repeat_condition: repeatIt
     },
+    ...tasks
   ]);
 };
