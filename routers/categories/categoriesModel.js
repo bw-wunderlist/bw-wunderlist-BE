6ea07374089b2b3d64d6fda13718e68392e2d844
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-17 20:11:13 -0400

    get categories by user_id done

--- routers/categories/categoriesModel.js
+++ routers/categories/categoriesModel.js
@@ -1,18 +1,11 @@
 const db = require("../../data/dbConfig.js");
 
 module.exports = {
-  getAllCategoriesById,
-  getCategoryById
+  getAllCategoriesById
 };
 
 function getAllCategoriesById(id) {
   return db("tasks")
     .where({ user_id: id })
-    .select("id", "name", "categories");
-}
-
-function getCategoryById(id) {
-  return db("tasks")
-    .where({ id: id })
-    .first();
+    .select("categories");
 }
--- routers/categories/categoriesRouter.js
+++ routers/categories/categoriesRouter.js
@@ -6,22 +6,11 @@ const Categories = require("./categoriesModel.js");
 
 // GET ALL CATEGORIES BY USER ID
 router.get("/", async (req, res) => {
-  const id = "DnuzdSXCtMgmRWDCRRE1iQ";
+  const id = req.decoded.subject;
+  console.log(id);
   try {
     const categories = await Categories.getAllCategoriesById(id);
     res.status(200).json(categories);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const userId = req.decoded.subject;
-  try {
-    const cata = await Categories.getCategoryById(id);
-    if (cata) {
-    }
   } catch (err) {
     res.status(500).json(err);
   }
